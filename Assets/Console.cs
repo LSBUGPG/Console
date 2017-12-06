bd857e38f68a0ae5597ea5253b9a24c0c164395f
Paul Sinnett <paul.sinnett@gmail.com>
2017-12-06 10:42:46 +0000

    Initial commit

---
+++ Assets/Console.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+
+public class Console : MonoBehaviour
+{
+    public List<string> lines = new List<string>();
+    public int maximumLines = 15;
+    Text console;
+    TextGenerator generator;
+
+    void Awake()
+    {
+        console = GetComponent<Text>();
+        generator = new TextGenerator();
+    }
+
+    public void AddLine(string line)
+    {
+        lines.Add(line);
+        if (lines.Count > maximumLines)
+        {
+            lines.RemoveAt(0);
+        }
+
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (string line in lines)
+        {
+            stringBuilder.AppendLine(line);
+        }
+
+        console.text = stringBuilder.ToString();
+    }
+
+    public IEnumerable<string> BreakMessageIntoLines(string message)
+    {
+        generator.Populate(message, console.GetGenerationSettings(console.rectTransform.sizeDelta));
+        UILineInfo previousLine = new UILineInfo();
+        bool first = true;
+        foreach (UILineInfo line in generator.lines)
+        {
+            if (!first)
+            {
+                int length = line.startCharIdx - previousLine.startCharIdx;
+                yield return message.Substring(previousLine.startCharIdx, length);
+            }
+            previousLine = line;
+            first = false;
+        }
+
+        yield return message.Substring(previousLine.startCharIdx);
+    }
+}
---
+++ Assets/Messages.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Messages : MonoBehaviour
+{
+    public string[] messages;
+    public Console console;
+
+    void Start()
+    {
+        StartCoroutine(Generate());
+    }
+
+    IEnumerator Generate()
+    {
+        while (true)
+        {
+            foreach (string message in messages)
+            {
+                foreach (string line in console.BreakMessageIntoLines(message))
+                {
+                    console.AddLine(line);
+                    yield return new WaitForSeconds(0.1f);
+                }
+
+                console.AddLine("");
+
+                yield return new WaitForSeconds(2);
+            }
+        }
+    }
+
+}
